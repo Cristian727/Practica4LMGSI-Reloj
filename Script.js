d6e9beb66d4aaee0b4b77343da6f0dc3710b0e2f
Cristian <cristian.gonzalez7@hotmail.com>
2023-11-19 17:11:43 +0100

    Reloj incompleto

---
+++ Script.js
@@ -0,0 +1,27 @@
+
+function select(tag){
+    const val = document.querySelector(tag)
+    return val
+  }
+  
+  function updateTag(tag, contenido){
+    const mi_tag = select(tag)
+    mi_tag.innerHTML = contenido
+  }
+  
+  function updateHour(){
+    
+  const now = new Date();
+  
+  const options = { hour: '2-digit', minute: '2-digit', second:'2-digit', hour12: false };
+  const horaEnFormato24Horas = now.toLocaleTimeString('es-ES', options);
+  const [hora, minuto, segundo] = horaEnFormato24Horas.split(":")
+  
+  
+  updateTag("#horas", hora)
+  updateTag("#minutos", minuto)
+  updateTag("#segundos", segundo)
+  }
+  
+  
+  setInterval(updateHour,500)
